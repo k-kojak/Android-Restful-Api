f47b89eb93e553006e46c299f85279052b8ff66a
Tamas Kojedzinszky <tamas.kojedzinszky@gmail.com>
2015-07-17 15:28:22 +0200

    Comment added.

--- src/main/java/hu/kojak/android/restservice/restapi/ApiAsyncTask.java
+++ src/main/java/hu/kojak/android/restservice/restapi/ApiAsyncTask.java
@@ -3,6 +3,9 @@ package hu.kojak.android.restservice.restapi;
 import android.annotation.TargetApi;
 import android.os.AsyncTask;
 import android.os.Build;
+import android.util.Log;
+
+import java.util.concurrent.RejectedExecutionException;
 
 /**
  * This class has an exec() method, which calls the appropriate execute() method to be API
@@ -13,11 +16,21 @@ public abstract class ApiAsyncTask<Params, Progress, Result>
 
   @TargetApi(11)
   @SuppressWarnings("unchecked")
-  public final void exec(Params... params) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-      executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, params);
-    } else {
-      execute(params);
+  /**
+   * Returns true if execution started without problem, false if cannot start asynctask because
+   * pool is full.
+   */
+  public final boolean exec(Params... params) {
+    try {
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+        executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, params);
+      } else {
+        execute(params);
+      }
+      return true;
+    } catch (RejectedExecutionException e) {
+      Log.d("kojak", "thread pool is full :(", e);
+      return false;
     }
   }
 }
